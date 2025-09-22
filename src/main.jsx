704a45dd362a222f19a576af82bc87a31f5c031a
SivaPrasad Reddy Devloper <evurisivaprasadreddy3@gmail.com>
2025-09-22 13:52:40 +0530

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Navbar from './components/navbar/navbar.jsx' // Component names should be PascalCase
 import Banner from './pages/banner.jsx'
 
@@ -9,7 +8,6 @@ import Banner from './pages/banner.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
-    <Banner /> 
-    
+    <Banner />
   </StrictMode>,
 )
